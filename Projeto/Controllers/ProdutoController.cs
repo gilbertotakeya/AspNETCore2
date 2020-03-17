2ce0296c45966b8b408bb5846b7b025ef2b63513
gilbertotakeya <gilbertotakeya@yahoo.com.br>
2020-03-17 20:52:25 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //[privado/public/protected] [TipoResultado] [Nome da Acao]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto/Models/ProdutoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.Models
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Preencha o campo Nome")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Descrição do produto")]
+        [StringLength(256, MinimumLength = 10, ErrorMessage ="O campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}", ApplyFormatInEditMode =true)]
+        [Display(Name ="Data de validade", Description = "Data de validade")]
+        [Required(ErrorMessage = "Data de validade")]
+        public DateTime DataValidade { get; set; }
+
+        [Required(ErrorMessage = "O campo valor é obrigatório")]
+        public double ValorVenda { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
